bc823a8a82da2f9b7620b9d3c7d15d3bc90ef2fa
Iris Lerner <ivis.lerner@gmail.com>
2017-08-24 22:43:02 +0300

    changes to ankaapp

--- ankaclients/assets/js/scripts.js
+++ ankaclients/assets/js/scripts.js
@@ -10,7 +10,7 @@
 
   'use strict';
 
-  var app = angular.module('AnkaApp', ['ngRoute'])
+  var app = angular.module('AnkaApp', ['ngRoute', 'rzModule'])
 
   app.config(['$locationProvider', function($locationProvider) {
         $locationProvider.hashPrefix('');
@@ -31,6 +31,9 @@
   		templateUrl: 'views/add-client.html'
 
   	})
+  	.when('/add-colouring/:client_id', {
+  		templateUrl: 'views/add-colouring.html'
+  	})
   	.otherwise({
     	redirectTo: '/'
   	});
@@ -42,6 +45,40 @@
 
   app.controller('ClientCardController', function($scope, $route, $routeParams){
   	this.client = clients[$routeParams.client_id];
+  });
+
+  app.controller('rangeController', function($scope){
+  	$scope.toneSlider = {
+  		value: 10, 
+  		step: 1,
+  		options:  {
+  			hideLimitLabels: true
+  		}
+  	}
+  	
+  	$scope.greySlider = {
+  		value: 100,
+  		step: 1,
+  		options:  {
+  			hideLimitLabels: true
+  		}
+  	}
+
+  	$scope.weightSlider = {
+  		value: 100,
+  		step:  1,
+  		options:  {
+  			hideLimitLabels: true
+  		}
+  	}
+
+  	$scope.percSlider = {
+  		value: 100,
+  		step: 1,
+  		options: {
+  			hideLimitLabels:true
+  		}
+  	}
   });
 
   app.controller('CategoryController', function(){
@@ -67,30 +104,61 @@
   app.controller("ColorCardController", function($scope,$route,$routeParams){
   	this.colouring = colourings[$routeParams.colouring_id];
   	this.client_name = $routeParams.client_name;
+
+
+
+
   });
 
+  app.controller('AddColouringController', function($scope,$route,$routeParams){
+  	 this.client_name = clients[$routeParams.client_id].name;
+
+  	 $('.add-color').click(function(e){
+
+  	 	var colorList = $(".color-info ul");
+  	 	var colorBlock = $("<li class='chem-element edit-mode'><span class='top'>N</span><span class='bottom'></span></li>");
+  	 	colorList.prepend(colorBlock);
+
+  	 });
+
+  	 $('.chem-element.edit-mode:after').click(function(e){
+  	 	this.removeElement();
+  	 });
+
+  });
+
+
+  /***************************************
+  					FILTERS 
+  ***************************************/
+  app.filter('weight_g',function(){
+
+  		return function(input) {
+  			return input + "гр."
+  		}
+  });
   /* data model*/ 
 
   /* clients */
   var clients = [
   	{ 
   		id:0, 
-  		name: "Иван Иванов", 
-  		img: "https://placeimg.com/50/50/people",
+  		name: "Марина Веселова", 
+  		img: "https://placeimg.com/40/40/people",
   		natural_tone: 7,
   		grey_perc: 5,
   		phone: "+9 999 999 99 99",
   		email: "example@example.com",
-  		category: "VIP",
-  		sex: "M",
-  		address: "Bat Yam",
+  		category: "",
+  		sex: "F",
+  		address: "",
   		fb_page: "http://facebook.com",
-  		other: "Лысый. Стричь нечего"
+  		other: ""
   	},
   	{ 
   		id:1, 
-  		name: "Анна Аннова", 
-  		img: "https://placeimg.com/50/50/people",
+  		name: "Мария Загацкая", 
+  		img: "https://placeimg.com/40/40/people",
   		natural_tone: 5,
   		grey_perc: 15,
   		phone: "+9 999 999 99 99",
@@ -99,22 +167,22 @@
   		sex: "F",
   		address: "Tel Aviv",
   		fb_page: "http://facebook.com",
-  		other: "Интересный собеседник"
+  		other: ""
   	
   	},
   	{ 
   		id:2, 
-  		name: "Ирина Иринова", 
-  		img: "https://placeimg.com/50/50/people",
+  		name: "Елена Охотникова", 
+  		img: "https://placeimg.com/40/40/people",
   		natural_tone: 8,
   		grey_perc: 25,
   		phone: "+9 999 999 99 99",
   		email: "example@example.com",
   		category: "",
   		sex: "F",
-  		address: "Moscow",
+  		address: "Бат Ям",
   		fb_page: "http://facebook.com",
-  		other: "Если стрижка не понравилась, бьет больно"
+  		other: ""
   	}  
   ];
 
@@ -142,13 +210,14 @@
   		{
   			id: 0,
   			client_id: 1,
-  			date: "2017-01-01",
-  			type: "Омбре",
+  			date: "2017-08-20",
+  			type: "Мелирование",
+  			price: 800,
   			colouring_data: {
   				oxid_perc: 10, 
   				oxid_weight_g: 20,
   				colors: [
-  					{name: "Wella 1", color_num: 8.6, color_weight_g: 20},
+  					{name: "wella blondor", color_num: 8.6, color_weight_g: 20},
   					{name: "Wella 1", color_num: 8.6, color_weight_g: 20}
   				]
   			}
@@ -159,6 +228,7 @@
   			client_id: 1,
   			date: "2015-05-22",
 			type: "Корни",
+			price: 300,
 			colouring_data: {}
   		}
 
@@ -172,6 +242,7 @@
 			id:0,
 			date: "2017-01-01",
 			type: "Омбре",
+			price: 800,
 			coloringData: {
 				oxid: {	name: "Oxilan" , perc: 10, weight_gr: 20 }, 
 				color: [{ name: "Wella 1", num: 8.6, weight_gr: 40 },
@@ -183,7 +254,8 @@
 			clientid: 1,
 			id:0,
 			date: "2015-05-22",
-			type: "Корни"
+			type: "Корни",
+			price: 500
 		}
 	];
 
